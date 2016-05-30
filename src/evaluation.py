5057be37fd18df78ab1eaf4ad4965832b80c1392
Thijs Scheepers <thijs@label305.com>
2016-05-30 18:40:38 +0200

    Added bleu diff break

--- src/evaluation.py
+++ src/evaluation.py
@@ -21,18 +21,22 @@ def best_reranking(inputs, candidates, references, classifier, normalizer, pca, 
     if limit < improve_limit / 4:
         improve_limit = int(limit / 4)
 
+    blue_diff = 0
+
     for i, input in enumerate(inputs):
 
-        if i > 0:
-            baseline_blue = corpus_bleu(references[:i], best_baseline(inputs[:i], candidates[:i]))
-            reranking_blue = corpus_bleu(references[:i], sentences)
-            blue_diff = reranking_blue - baseline_blue
-            sys.stdout.write("\rReranking %6.2f%% classifications: %d current BLEU diff: %0.5f" %
-                             ((100 * i) / float(len(inputs)), classifications, blue_diff))
-            sys.stdout.flush()
-        else:
-            sys.stdout.write("\rReranking %6.2f%% classifications: %d" % ((100 * i) / float(len(inputs)), classifications))
-            sys.stdout.flush()
+        baseline_blue = corpus_bleu(references[:i], best_baseline(inputs[:i], candidates[:i]))
+        reranking_blue = corpus_bleu(references[:i], sentences)
+        blue_diff = reranking_blue - baseline_blue
+
+        sys.stdout.write("\rReranking %6.2f%% classifications: %d current BLEU diff: %0.5f" %
+                                 ((100 * i) / float(len(inputs)), classifications, blue_diff))
+        sys.stdout.flush()
+
+        if i > 10:
+            if blue_diff < -0.028:
+                print("\rReranking broken off, BLEU diff to far down %0.5f" % blue_diff)
+                break
 
         results, classifications_ = best_reranking_sentence(candidates[i], input, classifier, normalizer, pca, limit, improve_limit)
         classifications += classifications_
@@ -40,7 +44,7 @@ def best_reranking(inputs, candidates, references, classifier, normalizer, pca, 
         (_, _, target) = results[0]
         sentences.append(target)
 
-    print("\rReranking 100%% classifications: %d" % classifications)
+    print("\rReranking finished classifications: %d" % classifications)
 
     return sentences
 
@@ -105,16 +109,18 @@ def best_baseline(inputs, candidates):
     return sentences
 
 
-def print_evaluation(inputs, references, candidates, classifier, normalizer, pca, limit=1000):
+def print_evaluation(inputs, references, candidates, classifier, normalizer, pca, limit=1000, improve_limit=10):
     bleu_references = [[x] for x in references]
     bleu_hypotheses_baseline = best_baseline(inputs, candidates)
 
     baseline_blue = corpus_bleu(bleu_references, bleu_hypotheses_baseline)
     print("Baseline BLEU: %0.10f" % baseline_blue)
 
-    bleu_hypotheses_reranking = best_reranking(inputs, candidates, bleu_references, classifier, normalizer, pca, limit)
-    reranking_blue = corpus_bleu(bleu_references, bleu_hypotheses_reranking)
-    print("Reranking BLEU: %0.10f" % reranking_blue)
+    bleu_hypotheses_reranking = best_reranking(inputs, candidates, bleu_references, classifier, normalizer, pca, limit, improve_limit)
 
-    blue_diff = reranking_blue - baseline_blue
-    print("BLEU Diff: %0.10f" % blue_diff)
+    if len(bleu_references) == len(bleu_hypotheses_reranking):
+        reranking_blue = corpus_bleu(bleu_references, bleu_hypotheses_reranking)
+        print("Reranking BLEU: %0.10f" % reranking_blue)
+
+        blue_diff = reranking_blue - baseline_blue
+        print("BLEU Diff: %0.10f" % blue_diff)
